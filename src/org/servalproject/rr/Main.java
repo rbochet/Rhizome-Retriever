7b171352c9287e69df55ff239420413e187377e7
Romain Bochet <rom@bochet.fr>
2011-09-11 00:55:28 +0200

    Displays the list of logical files.

--- src/org/servalproject/rr/Main.java
+++ src/org/servalproject/rr/Main.java
@@ -3,29 +3,55 @@ package org.servalproject.rr;
 import java.io.File;
 import java.io.FilenameFilter;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
- * Rhizome Retriever main activity
+ * Rhizome Retriever main activity. Extends ListActivity to be able to list the
+ * files in a table.
  * 
  * @author rbochet
  */
-public class Main extends Activity {
+public class Main extends ListActivity {
 
 	/** TAG for debugging */
 	public static final String TAG = "R2";
-
+	
+	String[] fList;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 
 		Log.v(TAG, "Launch the listing");
 		listFiles();
+		
+		
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item,
+				fList));
+
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		}); 
+
 	}
 
 	/**
@@ -48,19 +74,15 @@ public class Main extends Activity {
 			};
 
 			// List of the relative paths
-			String[] fList = path.list(filter);
-			// List of the RhizomeFile 
+			fList = path.list(filter);
+			// List of the RhizomeFile
 			RhizomeFile[] rList = new RhizomeFile[fList.length];
-			
+
 			for (int i = 0; i < rList.length; i++) {
 				rList[i] = new RhizomeFile(path, fList[i]);
 				Log.v(TAG, rList[i].toString());
 			}
-			
-			
-			
-			
-	
+
 		} else { // The pass does not exist
 			Log.e(TAG, "No serval-rhizome path found on the SD card.");
 		}
