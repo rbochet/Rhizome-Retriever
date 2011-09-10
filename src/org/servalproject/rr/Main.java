b173370af979eb874a435586d0dd02a0d7bcc9b6
Romain Bochet <rom@bochet.fr>
2011-09-10 23:11:01 +0200

    List all the files on /sdcard/serval-rhizome that are: - non hidden (ie. does not start w/ .) - actual file (ie non directory)

--- src/org/servalproject/rr/Main.java
+++ src/org/servalproject/rr/Main.java
@@ -1,13 +1,60 @@
 package org.servalproject.rr;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 
+/**
+ * Rhizome Retriever main activity
+ * 
+ * @author rbochet
+ */
 public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** TAG for debugging */
+	public static final String TAG = "R2";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Log.v(TAG, "Launch the listing");
+		listFiles();
+	}
+
+	/**
+	 * List files of the directory serval on the SD Card
+	 */
+	private void listFiles() {
+
+		File path = new File(Environment.getExternalStorageDirectory()
+				+ "/serval-rhizome");
+		Log.v(TAG, path.getAbsolutePath());
+
+		// If the path exists, list all the non-hidden files (no dir)
+		if (path.exists()) {
+			FilenameFilter filter = new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String filename) {
+					File sel = new File(dir, filename);
+					return (sel.isFile() && !sel.isHidden());
+				}
+			};
+
+			String[] fList = path.list(filter);
+			for (String rhizomeFile : fList) {
+				Log.v(TAG, "Found " + rhizomeFile);
+			}
+	
+		} else { // The pass does not exist
+			Log.e(TAG, "No serval-rhizome path found on the SD card.");
+		}
+
+	}
 }
